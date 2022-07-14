51a6f185414593e48e8b1a37df919f2351793329
Amit-172000 <kanhaiyayadav172000@gmail.com>
2022-07-14 15:45:05 +0530

    consoleapp4 commited

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace consoleapp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this is consoleapp4 for git build trigger configuration");
+        }
+    }
+}
